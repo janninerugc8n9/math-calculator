f83a464e57619d7822400b6c900cbee585352491
janninerugc8n9 <janninerugc8n9@gmx.com>
2025-03-04 16:14:03 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,18 @@
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "Enter first number: ";
+   int num1;
+   cin >> num1;
+
+   cout << "Enter second number: ";
+   int num2;
+   cin >> num2;
+
+   int result = num1 + num2;
+   cout << "The sum is: " << result << endl;
+
+   return 0;
+}
