6656e0768478704e367838428d395f985bcbd7ef
janninerugc8n9 <janninerugc8n9@gmx.com>
2025-03-05 16:46:20 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,36 @@
+// Function to perform addition
+int add(int num1, int num2) {
+  return num1 + num2;
+}
+
+// Function to perform subtraction
+int subtract(int num1, int num2) {
+  return num1 - num2;
+}
+
+// Function to perform multiplication
+int multiply(int num1, int num2) {
+  return num1 * num2;
+}
+
+// Function to perform division
+int divide(int num1, int num2) {
+  return num1 / num2;
+}
+
+// Driver code
+int main() {
+  // Testing the addition function
+  cout << add(5, 3) << endl;
+  
+  // Testing the subtraction function
+  cout << subtract(7, 3) << endl;
+  
+  // Testing the multiplication function
+  cout << multiply(4, 6) << endl;
+  
+  // Testing the division function
+  cout << divide(12, 4) << endl;
+  
+  return 0;
+}
